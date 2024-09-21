8823fe68b90d3b513b935b44c1c7f9e0db4b32d6
Igor souza <igor1_souza@hotmail.com>
2024-09-21 18:00:56 -0400

    tentando remover a restrição owner pros dados

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,14 +3,14 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 const schema = a.schema({
   Post: a.model({
     title: a.string().required(),
-    comments: a.hasMany('Comment', 'postId'),  // 'postId' é a chave estrangeira no modelo Comment
+    comments: a.hasMany('Comment', 'postId'), // 'postId' é a chave estrangeira no modelo Comment
     owner: a.string().authorization(allow => [allow.owner().to(['read', 'delete'])]),
   }).authorization(allow => [allow.publicApiKey().to(["read"]), allow.owner()]),
 
   Comment: a.model({
     postId: a.string().required(),
     content: a.string().required(),
-    post: a.belongsTo('Post', 'postId'),  // 'postId' é a chave estrangeira
+    post: a.belongsTo('Post', 'postId'), // 'postId' é a chave estrangeira
     owner: a.string().authorization(allow => [allow.owner().to(['read', 'delete'])]),
   }),
 
@@ -20,7 +20,9 @@ const schema = a.schema({
     size: a.string(),
     crs: a.string(),
     geometry: a.json(),
-  }).authorization(allow => [allow.publicApiKey().to(["read"]), allow.owner()]),
+  }).authorization(allow => [
+    allow.publicApiKey().to(["create", "read", "update", "delete"]) // Permite leitura pública sem restrição de proprietário
+  ]),
 }).authorization(allow => [allow.publicApiKey().to(["read"]), allow.owner()])
 
 export type Schema = ClientSchema<typeof schema>;
