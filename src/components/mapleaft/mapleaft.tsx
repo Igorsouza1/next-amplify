e288f0c1e742364f5fbc44a474e818297a38f138
igor <igor1_souza@hotmail.com>
2024-09-24 18:24:25 -0300

    ainda noa esta pronto a exibição dos dados

--- src/components/mapleaft/mapleaft.tsx
+++ src/components/mapleaft/mapleaft.tsx
@@ -14,7 +14,7 @@ import { LatLngTuple } from "leaflet";
 const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), { ssr: false });
 
 const MapLeaflet = () => {
-  const [polygonCoordinates, setPolygonCoordinates] = useState<LatLngTuple[][]>([]);
+  const [polygonCoordinates, setPolygonCoordinates] = useState<LatLngTuple[][][]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,12 +22,13 @@ const MapLeaflet = () => {
         // Obter os dados de geometria
         const geometryData = await getInitialGeometry();
         console.log("MapLeaflet: Dados de geometria obtidos:", geometryData);
-  
+
         // Verificar se o array de geometria e a string de geometria possuem dados
         if (geometryData && geometryData.length > 0 && geometryData[0]?.geometry) {
           const geometryString = geometryData[0].geometry;
+          console.log("MapLeaflet: String de geometria obtida:", geometryString);
           const coordinates = convertPolygonStringToLatLngTuples(geometryString as string);
-  
+
           if (coordinates.length > 0) {
             setPolygonCoordinates(coordinates);
           } else {
@@ -40,23 +41,21 @@ const MapLeaflet = () => {
         console.error("MapLeaflet: Erro ao buscar dados de geometria:", error);
       }
     };
-  
+
     fetchData();
   }, []);
-  
+
   return (
     <MapContainer center={[-21.327773, -56.694734]} zoom={10} style={{ height: "100vh", width: "100%" }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Polygon  positions={polygonCoordinates} color="blue" />
+      {polygonCoordinates.map((polygon, index) => (
+        <Polygon key={index} positions={polygon} color="blue" />
+      ))}
     </MapContainer>
   );
 };
 
 export default MapLeaflet;
-function fetchData() {
-  throw new Error("Function not implemented.");
-}
-
