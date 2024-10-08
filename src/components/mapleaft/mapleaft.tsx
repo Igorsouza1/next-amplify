3e77cb9b54e47497bc4c920d966229cbb33de102
igor <igor1_souza@hotmail.com>
2024-10-08 16:08:33 -0300

    adicionados outros estilos de mapa

--- src/components/mapleaft/mapleaft.tsx
+++ src/components/mapleaft/mapleaft.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { TileLayer } from "react-leaflet";
+import { TileLayer, LayersControl } from "react-leaflet"; // Importando LayersControl
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
@@ -15,6 +15,7 @@ const MapContainer = dynamic(
   { ssr: false }
 );
 
+const { BaseLayer } = LayersControl; // Alias para LayersControl.BaseLayer
 
 const MapLeaflet = () => {
   const { activeShapes } = useShapeContext();
@@ -25,21 +26,44 @@ const MapLeaflet = () => {
       zoom={10}
       style={{ height: "100vh", width: "100%" }}
     >
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {activeShapes.map((shape) =>
-        shape.features.map((feature) => (
-          <FeaturePolygon
-            key={feature.id}
-            feature={feature}
-            parentName={shape.name}
-            parentSize={shape.size}
-            parentColor={shape.color}
+      <LayersControl position="topright">
+        {/* Tile layers de diferentes fontes */}
+        <BaseLayer checked name="OpenStreetMap">
+          <TileLayer
+            attribution='&copy; OpenStreetMap contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        ))
-      )}
+        </BaseLayer>
+
+        <BaseLayer name="Esri World Imagery">
+          <TileLayer
+            attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          />
+        </BaseLayer>
+
+        <BaseLayer name="Topo Map">
+          <TileLayer
+            attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+            url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+          />
+        </BaseLayer>
+
+        {/* Você pode adicionar outras camadas base aqui */}
+
+        {/* Renderiza os polígonos (shapes) */}
+        {activeShapes.map((shape) =>
+          shape.features.map((feature) => (
+            <FeaturePolygon
+              key={feature.id}
+              feature={feature}
+              parentName={shape.name}
+              parentSize={shape.size}
+              parentColor={shape.color}
+            />
+          ))
+        )}
+      </LayersControl>
     </MapContainer>
   );
 };
